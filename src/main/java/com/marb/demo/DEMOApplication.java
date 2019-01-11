b62acfa22db8d1f137a422f629d87c022d5a07ee
Martin Ortega <martin.ortega@gmail.com>
2019-01-11 18:53:38 -0300

    Se agrega dockerfile + pluing para crear y deployar imagenes de docker con el proyecto

--- src/main/java/com/marb/demo/DEMOApplication.java
+++ src/main/java/com/marb/demo/DEMOApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.marb.demo","com.marb.framework.api"})
+@ComponentScan({ "com.marb.demo", "com.marb.framework.api" })
 public class DEMOApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/marb/demo/config/ApplicationConfiguration.java
+++ src/main/java/com/marb/demo/config/ApplicationConfiguration.java
@@ -3,9 +3,7 @@ package com.marb.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import com.marb.framework.api.filter.MdcFilter;
 
 @Configuration
 @EnableTransactionManagement
